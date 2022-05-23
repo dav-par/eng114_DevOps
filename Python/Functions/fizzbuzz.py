8bc24aef4e7ec2ac705eb815bdb20c04b0c0315b
dav-par <dav.par.code@gmail.com>
2022-05-23 17:33:25 +0100

    completed fizzbuzz for 0 to 100

---
+++ Python/Functions/fizzbuzz.py
@@ -0,0 +1,19 @@
+'''Fizzbuzz problem statement is very simple, you need to write a program that returns "fizz" if the number is a multiplier of 3, return "buzz" if its multiplier of 5, and return "fizzbuzz" if the number is divisible by both 3 and 5.'''
+
+
+#program that:
+#fizz for multiplier of 3
+#buzz for multiplier of 5
+#fizzbuzz if divisible by 3 and 5
+
+for i in range(0, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+
+    elif i % 3 == 0 and not i % 5 == 0:
+        print("fizz")
+
+    elif i % 5 == 0 and not i % 5 == 0:
+        print("buzz")
+
+    else: print(i)
