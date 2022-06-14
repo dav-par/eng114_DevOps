0b0d95d4d73ee31dfd9cc9b63582c7dbd31fd4bd
Dav Par <64789377+dav-par@users.noreply.github.com>
2022-06-14 09:25:45 +0100

    added enviroment variable

--- Python/boto3/ec2_setup.sh
+++ Python/boto3/ec2_setup.sh
@@ -5,7 +5,8 @@ sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.7 -y
 sudo apt install python3-pip -y
 sudo pip3 install awscli
-alias python=python3.7
+sudo echo "export python=python3.7" >> /home/ubuntu/.bashrc
+source ~/.bashrc
 python -m pip install boto3
 #aws configure
 #access key
