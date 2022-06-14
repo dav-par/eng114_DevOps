90c6a3b941a757edba1c8c23f86f3c8b7b598a9d
Dav Par <64789377+dav-par@users.noreply.github.com>
2022-06-14 09:04:53 +0100

    update

--- Python/boto3/ec2_setup.sh
+++ Python/boto3/ec2_setup.sh
@@ -3,7 +3,12 @@ sudo apt update
 sudo apt upgrade -y
 sudo add-apt-repository ppa:deadsnakes/ppa -y
 sudo apt install python3.7 -y
-alias python=python3.7
 sudo apt install python3-pip -y
 sudo pip3 install awscli
+alias python=python3.7
 python -m pip install boto3
+#aws configure
+#access key
+#secret key
+#eu-west-1
+#json
