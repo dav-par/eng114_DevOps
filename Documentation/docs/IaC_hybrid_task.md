0f86e594c6cdefc6816336bdcb7a2efbcc0117e6
Dav Par <64789377+dav-par@users.noreply.github.com>
2022-06-24 18:02:13 +0100

    hybrid task

---
+++ Documentation/docs/IaC_hybrid_task.md
@@ -0,0 +1,12 @@
+# IaC hybrid task
+controller local
+nodes online
+
+keys
+chmod
+folder structure
+
+
+- Set up the controller
+    - run the vagrant file and provisioning script
+        - [here](/IaC_ansible/)
---
+++ IaC_ansible/controller_pro_original.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# update and upgrade
+sudo apt update -y && sudo apt upgrade -y
+sudo apt-get update -y
+
+#install ansible	
+sudo apt-get install software-properties-common
+	
+sudo apt-add-repository ppa:ansible/ansible -y
+	
+sudo apt-get update
+	
+sudo apt-get install ansible -y
+
+sudo git clone https://github.com/dav-par/working_ansible.git /etc/ansible
+export ANSIBLE_HOST_KEY_CHECKING=False
+cd /etc/ansible
+ansible-playbook 1_nginx.yml
+ansible-playbook 2_proxy.yml
+ansible-playbook 3_nodejs.yml
+ansible-playbook 4_mongo.yml
+ansible-playbook 5_copy_app.yml
